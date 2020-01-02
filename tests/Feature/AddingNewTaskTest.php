765b51bb7e9393cf9f456c11b8abc2904d3d6d86
ahmed adel <abo3adel35@gmail.com>
2020-01-02 22:58:36 +0200

    test task will not be saved if no body provided

--- tests/Feature/AddingNewTaskTest.php
+++ tests/Feature/AddingNewTaskTest.php
@@ -8,5 +8,30 @@ use Tests\TestCase;
 
 class AddingNewTaskTest extends TestCase
 {
-    
+    use RefreshDatabase; // empty database after every task
+    use WithFaker; // use faker to produce fake data
+
+    public function testAnyOneCanCreateTask()
+    {
+        // create fake task body
+        $task = [
+            'body' => $this->faker->sentence
+        ];
+
+        // submit the new task form
+        $this->post('/', $task)
+            ->assertRedirect('/');
+
+        // check if new task was saved into database
+        $this->assertDatabaseHas('tasks', $task);
+
+        // visit homepage and see if this task was shown
+        $this->get('/')
+            ->assertSee($task['body']);
+    }
+
+    public function testSavingFailsIfNoTaskBodyProvided()
+    {
+        $this->post('/', [])->assertSessionHasErrors('body');
+    }
 }
