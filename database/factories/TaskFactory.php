e9291297e3d2e7a9bc3eada8a42b5847ee376015
ahmed adel <abo3adel35@gmail.com>
2020-01-02 20:55:13 +0200

    set task completed to false as default

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'body' => $faker->sentence,
-        'completed' => $faker->boolean
+        'completed' => false
     ];
 });
