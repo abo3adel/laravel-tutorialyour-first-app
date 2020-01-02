1720393ac05ad061736c5fdd3aff10a97044f498
ahmed adel <abo3adel35@gmail.com>
2020-01-02 20:32:02 +0200

    style task list

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,3 +1,25 @@
+{{-- inherit from app layout --}}
 @extends('layouts/app')
 
-@dump($tasks)
+@section('title')
+    All Tasks
+@endsection
+
+@section('content')
+
+    <table class="table table-striped table-horizontal mx-auto w-75 table-responsive text-white">
+        <tbody>
+            @foreach($tasks as $task)
+                <tr class="p-3">
+                    <td class="py-3 w-75 align-middle bg-primary ">{{$task->body}}</td>
+                    <td class="bg-primary"></td>
+                    <td class="py-3 bg-dark align-middle">
+                        <a href="#" class='btn btn-success m-'>Complete</a>
+                        <button class="btn btn-danger m-">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+@endsection
