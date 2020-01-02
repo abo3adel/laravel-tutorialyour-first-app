9d80f26c47b03bed693e794a40248623b4ef9826
ahmed adel <abo3adel35@gmail.com>
2020-01-02 19:47:02 +0200

    part2 finished

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->sentence,
+        'completed' => $faker->boolean
     ];
 });
--- database/seeds/TaskTableSeeder.php
+++ database/seeds/TaskTableSeeder.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Task;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TaskTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(TaskTableSeeder::class);
+        factory(Task::class, 7)->create();
     }
 }
